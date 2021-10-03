a8ad35c6910699f24139825b29fd6040fd947ade
MILTONSALAZAR2001 <87161156+MILTONSALAZAR2001@users.noreply.github.com>
2021-10-03 01:41:37 -0400

    Add files via upload

---
+++ app/src/main/java/com/example/infogram/adapter/CardViewAdapter.java
@@ -0,0 +1,102 @@
+package com.example.infogram.adapter;
+
+import android.app.Activity;
+import android.app.ActivityOptions;
+import android.content.Intent;
+import android.os.Build;
+import android.transition.Explode;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.core.app.ActivityOptionsCompat;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.infogram.R;
+import com.example.infogram.model.Image;
+import com.example.infogram.view.ImageDetailActivity;
+import com.squareup.picasso.Picasso;
+
+import org.w3c.dom.Text;
+
+import java.util.ArrayList;
+
+public class CardViewAdapter extends RecyclerView.Adapter <CardViewAdapter.CardViewHolder> {
+    //lista de imagenes
+    private ArrayList<Image> images;
+    private int resource;
+    private Activity activity;
+
+    public CardViewAdapter(ArrayList<Image> images,int resource , Activity activity){
+        this.images = images;
+        this.resource = resource;
+        this.activity = activity;
+    }
+
+    @NonNull
+    @Override
+    public CardViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+
+        View view = LayoutInflater.from(parent.getContext()).inflate(resource,parent,false);
+
+        return  new CardViewHolder(view);
+
+
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull CardViewHolder holder, int position) {
+        Image image = images.get(position);
+        //set imagen
+        Picasso.get().load(image.getUrlImage()).into(holder.imageCardView);
+
+        holder .usernameCardView.setText(image.getUsername());
+        holder .cantidadDiasCardView.setText(image.getCantidadDias());
+        holder .cantidadMeGustaCardView.setText(image.getCantidadMeGusta());
+        //onclicklistener
+        holder.imageCardView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(activity, ImageDetailActivity.class);
+                if (Build.VERSION.SDK_INT>= Build.VERSION_CODES.LOLLIPOP){
+                    Explode explode = new Explode();
+                    explode.setDuration(1000);
+                    activity.getWindow().setExitTransition(explode);
+                    activity.startActivity(intent, ActivityOptionsCompat.makeSceneTransitionAnimation(activity,view,activity.getString(R.string.transitionName_imageCardView)).toBundle());
+                }else {
+                    activity.startActivity(intent);
+
+                }
+
+            }
+        });
+
+
+    }
+
+    @Override
+    public int getItemCount() {
+
+        return images.size();
+    }
+
+    public class CardViewHolder extends RecyclerView.ViewHolder{
+        private ImageView imageCardView;
+        private TextView usernameCardView;
+        private TextView cantidadDiasCardView;
+        private TextView cantidadMeGustaCardView;
+
+        public CardViewHolder(@NonNull View itemView) {
+
+            super(itemView);
+
+            imageCardView = itemView.findViewById(R.id.imageCardView);
+            usernameCardView = itemView.findViewById(R.id.usernameCardView);
+            cantidadDiasCardView = itemView.findViewById(R.id.cantidadDiasCardView);
+            cantidadMeGustaCardView = itemView.findViewById(R.id.cantidadMeGustaCardView);
+        }
+    }
+}
---
+++ app/src/main/java/com/example/infogram/view/ImageDetailActivity.java
@@ -0,0 +1,24 @@
+package com.example.infogram.view;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.os.Bundle;
+
+import com.example.infogram.R;
+
+public class ImageDetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_image_detail);
+        showToolbar("",true);
+    }
+    public void showToolbar(String titulo,boolean botonSubir){
+        Toolbar toolbar = findViewById(R.id.toolbar_image_detail);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle(titulo);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(botonSubir);
+    }
+}
